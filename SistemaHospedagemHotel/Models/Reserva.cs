2bc2d58fd894ad85b2ac24e8e072c36089c01493
Ramon Dias <ramondiiaas@hotmail.com>
2024-09-21 16:34:02 -0300

    Resolução Desafio Sistema de Hospedagem

---
+++ SistemaHospedagemHotel/Models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A capacidade da suite é menor que o número de hóspedes recebido.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal ValorDiariaComDesconto = Suite.ValorDiaria - (Suite.ValorDiaria * 10 / 100);
+                valor = DiasReservados * (ValorDiariaComDesconto);
+            }
+
+            return valor;
+        }
+    }
+}
